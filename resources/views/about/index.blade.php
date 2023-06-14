94d74908fb90f9e55745354a1a5451b8c20733de
dev-wy-k <waiyankyw96@gmail.com>
2023-06-14 23:10:39 +0630

    lara 7 to 8 & smt else

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -9,33 +9,37 @@
                     <img src="{{ asset('images/about.jpg') }}" class="img-fluid" alt="">
                 </div>
 
-                <div class="d-flex justify-content-center"> 
+                <div class="d-flex justify-content-center">
                     <span class="ml-3 mt-3">
-                        <a href="https://www.facebook.com/v.lain.01/" target="_blank" title="Facebook" class="text-primary text-decoration-none">
+                        <a href="https://www.facebook.com/v.lain.01/" target="_blank" title="Facebook"
+                            class="text-primary text-decoration-none">
                             <i class="fab fa-facebook fa-2x"></i>
                         </a>
                     </span>
-                    
+
                     <span class="ml-3 mt-3">
-                        <a href="https://github.com/dev-wy-k" title="GitHub" target="_blank" class="text-primary text-decoration-none">
+                        <a href="https://github.com/dev-wy-k" title="GitHub" target="_blank"
+                            class="text-primary text-decoration-none">
                             <i class="fab fa-github fa-2x"></i>
                         </a>
                     </span>
-                    
+
                     <span class="ml-3 mt-3">
-                        <a href="https://www.linkedin.com/in/wai-yan-kyaw-dev/" target="_blank" title="LinkIn" class="text-primary text-decoration-none">
+                        <a href="https://www.linkedin.com/in/wai-yan-kyaw-dev/" target="_blank" title="LinkIn"
+                            class="text-primary text-decoration-none">
                             <i class="fab fa-linkedin fa-2x"></i>
                         </a>
                     </span>
-                    
+
                     <span class="ml-3 mt-3">
-                        <a href="https://dev-wy-k.github.io/Portfolio/" target="_blank" title="Website" class="text-primary text-decoration-none">
+                        <a href="https://dev-wy-k.github.io/Portfolio/" target="_blank" title="Website"
+                            class="text-primary text-decoration-none">
                             <i class="fas fa-globe-asia fa-2x"></i>
                         </a>
                     </span>
 
                 </div>
-            </div>            
+            </div>
         </div>
 
         <div class="col-12 col-lg-8 mb-5">
@@ -44,17 +48,23 @@
                 Wai Yan Kyaw
             </h2>
 
+            <?php
+            $currentYear = date('Y');
+            $startYear = 2021;
+            ?>
+
             <p class="text-white my-3 my-md-5">
-                I'm Learning Web Development for 1+ years ago and developed profile landing page and the most user-friendly website. <br>
-                This website is also developed with tmdb API and using laravel backend service, a little thing to do with user experience (UX) for alpine js and livewire. <br>
-                I want to create many projects for client needs so do you have to need something about the website? <br>  
+                I'm Learning Web Development for {{ $yearDifference = $currentYear - $startYear }}+ years ago and
+                developed profile landing page and the
+                most user-friendly website. <br>
+                This website is also developed with tmdb API and using laravel backend service, a little thing to do with
+                user experience (UX) for alpine js and livewire. <br>
+                I want to create many projects for client needs so do you have to need something about the website? <br>
             </p>
 
             <a href="https://t.me/dev_wyk" target="_blank" class="btn btn-primary text-white">You Can Hire Me!</a>
-            
+
         </div>
     </div>
-    <!-- Developer Info End  --> 
-       
+    <!-- Developer Info End  -->
 @endsection
-
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,98 +1,104 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{ asset('owl/dist/assets/owl.carousel.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('owl/dist/assets/owl.theme.default.css') }}">
-        <link rel="icon" href="{{ asset('favicon.ico') }}">
-        <link rel="stylesheet" href="/css/app.css">   
-        
-        <!-- Primary Meta Tags -->
-        <title>Movies Website</title>
-        <meta name="title" content="Movies Website">
-        <meta name="description" content="With Laravel and TMDB API Dev By WYK">
 
-        <!-- Open Graph / Facebook -->
-        <meta property="og:type" content="website">
-        <meta property="og:url" content="https://metatags.io/">
-        <meta property="og:title" content="Movies Website">
-        <meta property="og:description" content="With Laravel and TMDB API Dev By WYK">
-        <meta property="og:image" content="{{ asset('meta.png') }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('owl/dist/assets/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('owl/dist/assets/owl.theme.default.css') }}">
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+    <link rel="stylesheet" href="/css/app.css">
 
-        <!-- Twitter -->
-        <meta property="twitter:card" content="summary_large_image">
-        <meta property="twitter:url" content="https://metatags.io/">
-        <meta property="twitter:title" content="Movies Website">
-        <meta property="twitter:description" content="With Laravel and TMDB API Dev By WYK">
-        <meta property="twitter:image" content="{{ asset('meta.png') }}">
+    <!-- Primary Meta Tags -->
+    <title>Movies Website</title>
+    <meta name="title" content="Movies Website">
+    <meta name="description" content="With Laravel and TMDB API Dev By WYK">
 
-        <title>Movie App</title>
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://metatags.io/">
+    <meta property="og:title" content="Movies Website">
+    <meta property="og:description" content="With Laravel and TMDB API Dev By WYK">
+    <meta property="og:image" content="{{ asset('meta.png') }}">
 
-        @yield('head')
-        @livewireStyles
-    </head>
-    <body class="bg-dark">
-        <div class="container min-vh-100">
-            <div class="row py-2">
-                <nav class="navbar navbar-expand-lg navbar-dark w-100">
-                    <a class="navbar-brand mr-0 mr-md-3" href="{{ route('movie.index') }}">
-                        <div class="d-flex align-items-center">
-                            <i class="fas fa-compact-disc fa-2x text-primary mr-2"></i>
-                            <span class="text-primary h4 mb-0">Movie App</span>
-                        </div>
-                    </a>
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://metatags.io/">
+    <meta property="twitter:title" content="Movies Website">
+    <meta property="twitter:description" content="With Laravel and TMDB API Dev By WYK">
+    <meta property="twitter:image" content="{{ asset('meta.png') }}">
 
+    <title>Movie App</title>
+
+    @yield('head')
+    @livewireStyles
+</head>
+
+<body class="bg-dark">
+    <div class="container min-vh-100">
+        <div class="row py-2">
+            <nav class="navbar navbar-expand-lg navbar-dark w-100">
+                <a class="navbar-brand mr-0 mr-md-3" href="{{ route('movie.index') }}">
                     <div class="d-flex align-items-center">
-                        <div class="d-block d-lg-none">
-                            <livewire:search-dropdown /> 
-                        </div>
-    
-                        <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>  
-                    </div>                  
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link text-primary" href="{{ route('movie.index') }}">Movie</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-primary" href="{{ route('tv.index') }}">TV Shows</a>
-                            </li> 
-                            <li class="nav-item">
-                                <a class="nav-link text-primary" href="{{ route('actors.index') }}">Actors</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-primary" href="{{ route('about.index') }}">About</a>
-                            </li>                 
-                        </ul>
-                        <div class="ml-auto d-none d-lg-block">
-                            <livewire:search-dropdown /> 
-                        </div>
+                        <i class="fas fa-compact-disc fa-2x text-primary mr-2"></i>
+                        <span class="text-primary h4 mb-0">Movie App</span>
                     </div>
-                </nav>
-            </div>
-                     <hr>           
-            <div class="row py-4 py-lg-5">                
-                <div class="container">                        
-                    @yield('content')
-                </div>
-            </div>
+                </a>
 
-            <div class="text-center text-white p-2">
-                <p>
-                    Copy Right © 2022 <a href="{{ route('movie.index') }}"class="text-white text-decoration-none">Movie App</a> |
-                    Created by <a href="{{ route('about.index') }}" class="text-white text-decoration-none">Wai
-                        Yan Kyaw</a>
-                </p>
+                <div class="d-flex align-items-center">
+                    <div class="d-block d-lg-none">
+                        <livewire:search-dropdown />
+                    </div>
+
+                    <button class="navbar-toggler border-0" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link text-primary" href="{{ route('movie.index') }}">Movie</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-primary" href="{{ route('tv.index') }}">TV Shows</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-primary" href="{{ route('actors.index') }}">Actors</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-primary" href="{{ route('about.index') }}">About</a>
+                        </li>
+                    </ul>
+                    <div class="ml-auto d-none d-lg-block">
+                        <livewire:search-dropdown />
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <hr>
+        <div class="row py-4 py-lg-5">
+            <div class="container">
+                @yield('content')
             </div>
         </div>
-        
-        @livewireScripts
-        <script src="/js/app.js"></script>
-        <script src="{{ asset('owl/dist/owl.carousel.min.js') }}"></script>
-        @yield('foot')
-    </body>
+
+        <div class="text-center text-white p-2">
+            <p>
+                Copy Right © {{ date('Y') }} <a
+                    href="{{ route('movie.index') }}"class="text-white text-decoration-none">Movie App</a> |
+                Created by <a href="{{ route('about.index') }}" class="text-white text-decoration-none">Wai
+                    Yan Kyaw</a>
+            </p>
+        </div>
+    </div>
+
+    @livewireScripts
+    <script src="/js/app.js"></script>
+    <script src="{{ asset('owl/dist/owl.carousel.min.js') }}"></script>
+    @yield('foot')
+</body>
+
 </html>
